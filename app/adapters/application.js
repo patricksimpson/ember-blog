3a5c15f1f5b51e5bec3683bb1e39f93bc941a79d
patricksimpson <izerop@gmail.com>
2015-03-20 00:50:11 -0400

    feat: added deploy updated api

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 import ApplicationAdapter from './application';
 
 export default ApplicationAdapter = DS.RESTAdapter.extend({
-  host: 'http://pks.dev',
+  host: 'http://api.patricksimpson.me',
   namespace: 'wp-json'
 });
---
+++ config/deploy.rb
@@ -0,0 +1,64 @@
+require 'mina/bundler'
+require 'mina/rails'
+require 'mina/git'
+# require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
+# require 'mina/rvm'    # for rvm support. (http://rvm.io)
+
+# Basic settings:
+#   domain       - The hostname to SSH to.
+#   deploy_to    - Path to deploy into.
+#   repository   - Git repo to clone from. (needed by mina/git)
+#   branch       - Branch name to deploy. (needed by mina/git)
+
+set :domain, 'patricksimpson.me'
+set :deploy_to, '/var/www/patricksimpson.me'
+set :repository, 'git@github.com:patricksimpson/ember-blog.git'
+set :branch, 'master'
+
+# For system-wide RVM install.
+#   set :rvm_path, '/usr/local/rvm/bin/rvm'
+
+# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
+# They will be linked in the 'deploy:link_shared_paths' step.
+set :shared_paths, []
+
+# Optional settings:
+  set :user, 'patrick'    # Username in the server to SSH to.
+  # set :port, '30000'     # SSH port number.
+  set :forward_agent, true     # SSH forward_agent.
+  set :term_mode, :system
+
+# This task is the environment that is loaded for most commands, such as
+# `mina deploy` or `mina rake`.
+task :environment do
+end
+
+# Put any custom mkdir's in here for when `mina setup` is ran.
+# For Rails apps, we'll make some of the shared paths that are shared between
+# all releases.
+task :setup => :environment do
+  queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/database.yml'."]
+end
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  deploy do
+    # Put things that will set up an empty directory into a fully set-up
+    # instance of your project.
+    invoke :'git:clone'
+    invoke :'deploy:cleanup'
+
+    to :launch do
+      # queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
+      # queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
+    end
+  end
+end
+
+# For help in making your deploy script, see the Mina documentation:
+#
+#  - http://nadarei.co/mina
+#  - http://nadarei.co/mina/tasks
+#  - http://nadarei.co/mina/settings
+#  - http://nadarei.co/mina/helpers
+
