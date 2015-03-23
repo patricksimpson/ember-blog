e1b46e66801ddceb8f68f1a73c9cff45edd2c7c2
patricksimpson <izerop@gmail.com>
2015-03-22 23:57:57 -0400

    feat: pagination

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,5 +3,10 @@ import ApplicationAdapter from './application';
 
 export default ApplicationAdapter = DS.RESTAdapter.extend({
   host: 'http://api.patricksimpson.me',
-  namespace: 'wp-json'
+  namespace: 'wp-json',
+  ajaxSuccess: function(jqXHR, jsonPayload) {
+    var totalPages = jqXHR.getResponseHeader('X-WP-TotalPages');
+    jsonPayload.meta = {total_pages: totalPages}
+    return jsonPayload;
+  }
 });
--- app/routes/posts.js
+++ app/routes/posts.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
+import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
 
-export default Ember.Route.extend({
-  model: function() {
-    return this.store.find('post');
+export default Ember.Route.extend(RouteMixin, {
+  model: function(params) {
+    params.paramMapping = { total_pages: "X-WP-TotalPages" };
+    return this.findPaged('post',params);
   }
 });
